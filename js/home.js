7d40e1bbf1aa9af82d4f49bc2b06fe6b48d0d7f3
Lujing Cen <bobbyluig@gmail.com>
2015-05-21 21:46:50 -0700

    blur for chrome

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	$('html').removeClass('preload');
 	$('.button-collapse').sideNav();
-	$('#z').css({'color': '#76FF03', 'filter': 'blur(0px)'});
+	$('#z').css({'color': '#76FF03', 'filter': 'blur(0px)', '-webkit-filter': 'blur(0px)'});
 });
--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("html").removeClass("preload"),$(".button-collapse").sideNav(),$("#z").css({color:"#76FF03",filter:"blur(0px)"})});
+$(document).ready(function(){$("html").removeClass("preload"),$(".button-collapse").sideNav(),$("#z").css({color:"#76FF03",filter:"blur(0px)","-webkit-filter":"blur(0px)"})});
